6c697e9ce9fb0257b6bd71cff67d44eda6cbd804
Brent <brent@Zipcoders-MacBook-Pro-3.local>
2023-06-29 12:11:09 -0400

    code written. Alice or Bob greeted. Other users are made aware they are invalid.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,22 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+        Scanner scanner = new Scanner(System.in);
+        String userName = "";
 
+        System.out.println("Enter name");
+        userName = scanner.nextLine();
+
+        if (userName.toUpperCase().equals("ALICE") || userName.toUpperCase().equals("BOB")){
+            System.out.println("Welcome " + userName.toUpperCase() + " ");
+        } else {
+            System.out.println("Invalid User. Goodbye.");
+        }
     }
 }
